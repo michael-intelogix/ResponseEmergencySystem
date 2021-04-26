42acce8c1338186ce30a7b883d58459321c3b2e2
XimenaMendozaIntelogix <81635067+XimenaMendozaIntelogix@users.noreply.github.com>
2021-04-26 10:02:58 -0600

    Revert "Prueba"
    This reverts commit c5020767d5ae83b51f83e41b469c702e089004ae.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -171,16 +171,6 @@ namespace ResponseEmergencySystem.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type DevExpress.Utils.Svg.SvgImage.
-        /// </summary>
-        internal static DevExpress.Utils.Svg.SvgImage logoutWhite {
-            get {
-                object obj = ResourceManager.GetObject("logoutWhite", resourceCulture);
-                return ((DevExpress.Utils.Svg.SvgImage)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap preview_16x16 {
