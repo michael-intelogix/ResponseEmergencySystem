756c3a8c8b1e467302fb944217a5b5e679a35e3e
michael reyes <michaelreyesfernandez@hotmail.com>
2021-07-16 08:44:58 -0600

    driver builder

---
+++ Builders/DriverBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseEmergencySystem.Builders
+{
+
+    public class Driver
+    {
+        public Guid ID_Driver { get; }
+        public string ID_Samsara { get; }
+        public string Name { get; }
+        public string LastName1 { get; }
+        public string LastName2 { get; }
+        public string PhoneNumber { get; }
+        public string License { get; }
+        public string ID_StateOfExpedition { get; }
+        public string State { get; }
+        public DateTime? ExpirationDate { get; set; }
+
+        public bool dSamsara;
+
+    }
+
+    class DriverBuilder
+    {
+    }
+}
